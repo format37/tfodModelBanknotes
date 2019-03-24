7a7946c26044a1af71404af99e99db3bae7a6df0
Alexey Yurasov <format37@gmail.com>
2019-03-24 19:27:29 +0300

    Add files via upload

---
+++ inference.sh
@@ -0,0 +1,1 @@
+python3 export_inference_graph.py --input_type image_tensor --pipeline_config_path training/ssd_mobilenet_v1_coco.config --trained_checkpoint_prefix training/model.ckpt-200000 --output_directory banknotes_inference_graph
