096b6addeefef80bbb5db21ad245927cc893d996
Alexey Yurasov <format37@gmail.com>
2019-03-29 20:25:36 +0300

    Add files via upload

---
+++ inference.sh
@@ -0,0 +1,3 @@
+#From tensorflow/models/research/
+#export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+python3 export_inference_graph.py --input_type image_tensor --pipeline_config_path /home/alex/.local/lib/python3.6/site-packages/tensorflow/models/research/object_detection/training_v2b/ssd_mobilenet_v1_coco.config --trained_checkpoint_prefix /home/alex/.local/lib/python3.6/site-packages/tensorflow/models/research/object_detection/training_v2b/model.ckpt-68407 --output_directory /home/alex/.local/lib/python3.6/site-packages/tensorflow/models/research/object_detection/banknotes_inference_graph_v2b
