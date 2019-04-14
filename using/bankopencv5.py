f7017649cf02ee27e2ec7aa38240b7c59333b90e
format37 <format37@gmail.com>
2019-04-14 20:40:14 +0300

    Changes to be committed: 	modified:   using/bankopencv5.py 	modified:   using/monitoring.py  Untracked files: 	using/images/

--- using/bankopencv5.py
+++ using/bankopencv5.py
@@ -15,6 +15,7 @@ from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 
+import pymssql
 import cv2
 capAddress="rtsp://admin:V35XB3Uz@10.0.4.40:554/live/main"
 sys.path.append("../../..")
@@ -47,11 +48,19 @@ def main(argv):
 	
 	os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
 	os.environ["CUDA_VISIBLE_DEVICES"]=str(ops_gpu)
-
+	
+	score_limit	= 0.1
 	PATH_TO_CKPT = '../../banknotes_inference_graph_v3_20904/frozen_inference_graph.pb'
 	PATH_TO_LABELS = '../training/object-detection.pbtxt'
 	NUM_CLASSES = 1
-
+	imagesBoxedDirectory	= "images/boxed/"								
+	contentSaveDirectory	= "/var/www/html/"
+	if not os.path.exists(imagesBoxedDirectory):
+		os.makedirs(imagesBoxedDirectory)
+	conn = pymssql.connect(server='10.2.4.25', user='ICECORP\\1csystem', password='0dKasn@ms+', database='shopEvents')
+	cursor = conn.cursor()
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	
 	detection_graph = tf.Graph()
 	with detection_graph.as_default():
 		od_graph_def = tf.GraphDef()
@@ -69,11 +78,11 @@ def main(argv):
 	with detection_graph.as_default():
 		with tf.Session(graph=detection_graph) as sess:
 			while True:
-				secondcurrent	= int(datetime.datetime.now().strftime("%S"))
+				dtnow=datetime.datetime.now()
+				secondcurrent	= int(dtnow.strftime("%S"))
 				if secondcurrent!=secondlast:
 					secondlast=secondcurrent			
 					is_it_my_time=np.count_nonzero(np.where(time_offsets==secondcurrent))
-					#print(str(secondcurrent)+"."+str(is_it_my_time))
 					if is_it_my_time:
 						cap = cv2.VideoCapture(capAddress)
 						ret, image_np = cap.read()
@@ -84,7 +93,8 @@ def main(argv):
 						classes = detection_graph.get_tensor_by_name('detection_classes:0')
 						num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 						(boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections],feed_dict={image_tensor: image_np_expanded})
-
+						final_score = np.squeeze(scores)
+						
 						vis_util.visualize_boxes_and_labels_on_image_array(
 							image_np,
 							np.squeeze(boxes),
@@ -92,8 +102,34 @@ def main(argv):
 							np.squeeze(scores),
 							category_index,
 							use_normalized_coordinates=True,
-							line_thickness=8)
-						#cv2.imshow('object detection '+str(ops_gpu), cv2.resize(image_np, (640,480)))
+							line_thickness=2)
+						
+						#get objects detected count
+						objectsDetectedCount	= 0
+						score_summ	= 0
+						for i in range(100):
+							if scores is None or final_score[i] > score_limit:
+								objectsDetectedCount=objectsDetectedCount+1
+								score_summ	= score_summ+final_score[i]
+						
+						#save for monitoring						
+						cv2.putText(image_np,"GPU-"+str(ops_gpu)+":"+str(objectsDetectedCount),(10,150), font, 1,(255,255,255),2,cv2.LINE_AA)
+						cv2.imwrite(imagesBoxedDirectory+dtnow.strftime("%Y-%m-%d_%H_%M_%S")+".jpg", image_np)
+
+						#save for report
+						if objectsDetectedCount>2:
+							toSaveDay=dtnow.strftime("%Y-%m-%d")
+							imagesBoxedDirectory	= contentSaveDirectory+"events/"+toSaveDay+"/boxed/"							
+							boxedImagePath	= imagesBoxedDirectory	+ dtnow.strftime("%Y-%m-%d_%H_%M_%S")+"_obj_"+str(objectsDetectedCount)+".jpg"
+							cv2.imwrite(boxedImagePath, image_np)
+							print(boxedImagePath)
+						
+							#save to sql
+							score_summString='%.2f'%(score_summ/objectsDetectedCount)#middle
+							cursor.execute("INSERT INTO events (eventDate,objectsCount,middleScore,FileName) VALUES ('"+dtnow.strftime("%Y-%m-%dT%H:%M:%S")+"',"+str(objectsDetectedCount)+","+score_summString+",'"+boxedImagePath+"')")
+							conn.commit()
+						
+						
 						print(str(ops_gpu)+"."+str(secondcurrent))
 							
 						if cv2.waitKey(25) & 0xFF == ord('q'):
--- using/monitoring.py
+++ using/monitoring.py
@@ -2,11 +2,15 @@ import cv2
 import os
 import numpy as np
 imagesPath="images/boxed/"
+cv2.namedWindow('monitoring', cv2.WINDOW_NORMAL)
 
-files = sorted(os.listdir(imagesPath))
 while True:
-	if np.count_nonzero(files):
-		img = cv2.imread(files[0])
-		cv2.imshow("monitoring", cv2.resize(img, (640,480)))
-		print(files[0])
+	files = sorted(os.listdir(imagesPath))
+	if np.count_nonzero(files)>2:
+		print(imagesPath+files[0])
+		img = cv2.imread(imagesPath+files[0])
+		cv2.imshow("monitoring", img)
 		os.unlink(imagesPath+files[0])
+		if cv2.waitKey(25) & 0xFF == ord('q'):
+			cv2.destroyAllWindows()
+			break
